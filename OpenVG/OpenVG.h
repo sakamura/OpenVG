85ec08d2e5cc9d86bf408bfbf23c162aaea2c738
sakamura <michel@micheldonais.com>
2016-08-18 10:11:09 -0400

    Initial Commit

---
+++ OpenVG/OpenVG.h
@@ -0,0 +1,13 @@
+//
+//  OpenVG.h
+//  OpenVG
+//
+//  Created by Michel Donais on 16-08-18.
+//  Copyright © 2016 Michel Donais. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OpenVG : NSObject
+
+@end
---
+++ OpenVG/OpenVG.m
@@ -0,0 +1,13 @@
+//
+//  OpenVG.m
+//  OpenVG
+//
+//  Created by Michel Donais on 16-08-18.
+//  Copyright © 2016 Michel Donais. All rights reserved.
+//
+
+#import "OpenVG.h"
+
+@implementation OpenVG
+
+@end
