ae80247825ac7d7b60d6f6b6dbf43c662fa86e40
Michel Donais <sakamura@users.noreply.github.com>
2016-09-01 11:34:02 -0400

    Removed XCode (C). Clarified OPENVG_VERSION.
    Removed auto-generated XCode copyrights (Which doesn't apply here) and changed version to reflect the revision. This value will get incresed every time there's a new version

--- OpenVG.c
+++ OpenVG.c
@@ -2,8 +2,5 @@
 //  OpenVG.c
 //  OpenVG
 //
-//  Created by Michel Donais on 16-08-18.
-//  Copyright © 2016 Michel Donais. All rights reserved.
-//
 
-const char* const OPENVG_VERSION = "1.1 w/MonkVG Extensions";
+const char* const OPENVG_VERSION = "1.1 rev 1";
